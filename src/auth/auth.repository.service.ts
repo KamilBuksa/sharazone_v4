fec98a6dfa1117fbd699ec9a691eb56ba6c911d9
Kamil <buksakowskikamil@gmail.com>
2022-02-11 10:26:03 +0100

    transfer all methods from auth.service.ts to auth.repository.service.ts

--- src/auth/auth.repository.service.ts
+++ src/auth/auth.repository.service.ts
@@ -1,22 +1,57 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Auth } from './entities/auth.entity';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { AuthDto } from './dto';
+import { Tokens } from './types';
+import * as argon from 'argon2';
 
 // AuthService ma być zapisany jako rozszerzenie AuthRepository, tak by metody były w AuthRepository a w AuthService wywołania
 @Injectable()
 export class AuthRepository {
   constructor(
     @InjectRepository(Auth)
-    public  authRepository: Repository<Auth>,
-    public  jwtService: JwtService,
+    protected readonly authRepository: Repository<Auth>,
+    protected readonly jwtService: JwtService,
   ) {
 
   }
+  async _signupLocal(dto: AuthDto): Promise<Tokens> {
+    const hash = await argon.hash(dto.password)
+    const newUser = await this.authRepository.save({
+      email: dto.email,
+      hash
+    })
+    const tokens = await this.geTokens(newUser.id, newUser.email);
+    await this.updateRtHash(newUser.id, tokens.refresh_token)
 
-  async log(req:Request):Promise<any> {
+
+    return tokens
+  }
+
+  async  _signinLocal(dto: AuthDto): Promise<Tokens> {
+    //metoda findUserByEmail
+    const data = await this.authRepository.createQueryBuilder().where({email: dto.email});
+    const userEmail = data.expressionMap.parameters.orm_param_0;
+
+    const user = await this.authRepository.findOne({email: userEmail});
+    if (!user) throw new ForbiddenException("Access Denied!");
+
+    const passwordMatches = await argon.verify(user.hash, dto.password);
+    if (!passwordMatches) throw new ForbiddenException("Access Denied!");
+
+    const tokens = await this.geTokens(user.id, user.email);
+    await this.updateRtHash(user.id, tokens.refresh_token);
+
+
+    return tokens
+
+  }
+
+
+  async _logout(req:Request):Promise<any> {
     console.log('działa');
     const barerToken = req.headers.authorization;
     const token = barerToken.substring(7, barerToken.length)
@@ -34,4 +69,43 @@ export class AuthRepository {
 
     return true
   }
+
+
+  async updateRtHash(userId: number, rt: string) {
+    const hash = await argon.hash(rt)
+    await this.authRepository.createQueryBuilder()
+      .update()
+      .set({hashedRt: hash})
+      .where({id: userId})
+      .execute()
+  }
+
+
+  async geTokens(userId: number, email: string): Promise<Tokens> {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync({
+        sub: userId,
+        email,
+      }, {
+        // secret: process.env.REFRESH_SECRET_TOKEN,
+        secret: 'at-secret',
+        expiresIn: 60 * 15,
+      }),
+      this.jwtService.signAsync({
+        sub: userId,
+        email,
+      }, {
+        // secret: process.env.REFRESH_SECRET_TOKEN,
+        secret: 'rt_secret',
+        expiresIn: 60 * 60 * 24 * 7,
+      })
+
+    ])
+
+    return {
+      access_token: at,
+      refresh_token: rt
+    }
+  }
+
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,116 +1,38 @@
-import {ForbiddenException, Injectable, Req} from '@nestjs/common';
-import {AuthDto} from "./dto";
-import {InjectRepository} from "@nestjs/typeorm";
-import {Auth} from "./entities/auth.entity";
+import { ForbiddenException, Injectable, Req } from '@nestjs/common';
+import { AuthDto } from './dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Auth } from './entities/auth.entity';
 import * as argon from 'argon2';
-import {Repository} from "typeorm";
-import {Tokens} from "./types";
-import {JwtService} from "@nestjs/jwt";
-import {Request} from "express";
+import { Repository } from 'typeorm';
+import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
 import { AuthRepository } from './auth.repository.service';
 
 @Injectable()
-export class AuthService extends AuthRepository{
-    constructor(
-        @InjectRepository(Auth)
-        public  authRepository: Repository<Auth>,
-        public  jwtService: JwtService
-    ) {
-        super(authRepository,jwtService)
-    }
+export class AuthService extends AuthRepository {
+  constructor(
+    @InjectRepository(Auth)
+    protected readonly authRepository: Repository<Auth>,
+    protected readonly jwtService: JwtService,
+  ) {
+    super(authRepository, jwtService);
+  }
 
-    async signupLocal(dto: AuthDto): Promise<Tokens> {
-        const hash = await argon.hash(dto.password)
-        const newUser = await this.authRepository.save({
-            email: dto.email,
-            hash
-        })
-        const tokens = await this.geTokens(newUser.id, newUser.email);
-        await this.updateRtHash(newUser.id, tokens.refresh_token)
+  async signupLocal(dto: AuthDto): Promise<Tokens> {
+     return this._signupLocal(dto)
+  }
 
+  async signinLocal(dto: AuthDto): Promise<Tokens> {
+    return this._signinLocal(dto);
+  }
 
-        return tokens
-    }
+  async logout(req: Request): Promise<boolean> {
+    return this._logout(req);
+  }
 
-    async  signinLocal(dto: AuthDto): Promise<Tokens> {
-        //metoda findUserByEmail
-        const data = await this.authRepository.createQueryBuilder().where({email: dto.email});
-        const userEmail = data.expressionMap.parameters.orm_param_0;
+  refreshToken() {
+  }
 
-        const user = await this.authRepository.findOne({email: userEmail});
-        if (!user) throw new ForbiddenException("Access Denied!");
-
-        const passwordMatches = await argon.verify(user.hash, dto.password);
-        if (!passwordMatches) throw new ForbiddenException("Access Denied!");
-
-        const tokens = await this.geTokens(user.id, user.email);
-        await this.updateRtHash(user.id, tokens.refresh_token);
-
-
-        return tokens
-
-    }
-
-    async logout(req: Request):Promise<boolean>  {
-       // await this.log(req)
-      //   const barerToken = req.headers.authorization;
-      //   const token = barerToken.substring(7, barerToken.length)
-      //   const decoded = this.jwtService.decode(token, {complete: true});
-      //   const payloadSub = decoded['payload'].sub;
-      //
-      // await this.authRepository.createQueryBuilder()
-      //       .update()
-      //       .set({hashedRt: null})
-      //       .where({
-      //           id: payloadSub,
-      //           // hashedRt: {not: null}   ,  coś z tym nie działa(?)
-      //       })
-      //       .execute()
-      //
-      //   return true
-
-        return this.log(req)
-    }
-
-    refreshToken() {
-    }
-
-
-    async updateRtHash(userId: number, rt: string) {
-        const hash = await argon.hash(rt)
-        await this.authRepository.createQueryBuilder()
-            .update()
-            .set({hashedRt: hash})
-            .where({id: userId})
-            .execute()
-    }
-
-
-    async geTokens(userId: number, email: string): Promise<Tokens> {
-        const [at, rt] = await Promise.all([
-            this.jwtService.signAsync({
-                sub: userId,
-                email,
-            }, {
-                secret: process.env.REFRESH_SECRET_TOKEN,
-                // secret: 'at-secret',
-                expiresIn: 60 * 15,
-            }),
-            this.jwtService.signAsync({
-                sub: userId,
-                email,
-            }, {
-                secret: process.env.REFRESH_SECRET_TOKEN,
-                // secret: 'rt_secret',
-                expiresIn: 60 * 60 * 24 * 7,
-            })
-
-        ])
-
-        return {
-            access_token: at,
-            refresh_token: rt
-        }
-    }
 
 }
